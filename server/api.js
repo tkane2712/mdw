a67ea038a628ec62beb9cf50cb8cd0d5a8b6dab2
Thomas Kane <tomkane@Obama.local>
2019-01-28 11:51:12 -0700

    config -> keys

--- server/api.js
+++ server/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const Twit = require("twit");
-const config = require("./config");
+const config = require("./keys");
 const T = new Twit(config);
 
 function randomNumber(limit) {
