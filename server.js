38101d407d97cb796b4af089bfd4cfd1d9b99de9
Thomas Kane <tomkane@Obama.local>
2019-01-28 12:55:43 -0700

    moveed server out

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const api = require("./api");
+const api = require("./server/api");
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use("/", api);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname + "../client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
