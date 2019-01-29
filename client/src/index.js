286b96c68a36865a7e468442d26cf3830fdd7fc5
Thomas Kane <tomkane@Obama.local>
2019-01-28 20:28:51 -0700

    better grid implementation

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-console.log("tom", document.getElementById("root"));
-
 ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
